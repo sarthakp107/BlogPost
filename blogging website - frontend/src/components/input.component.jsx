62ada5b31700bfe083cecf3f725f2ca2d9672f7f
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-02-09 19:13:39 +1100

    eye toggle password visibility

--- blogging website - frontend/src/components/input.component.jsx
+++ blogging website - frontend/src/components/input.component.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { FaUser } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import '@flaticon/flaticon-uicons/css/all/all.css';
 
-export const InputBox = ({name , type, id, value, placeholder}) => {
+export const InputBox = ({ name, type, id, value, placeholder }) => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
   return (
     <div className='relative w-[100%] mb-4'>
-        <input type={type} 
+      <input 
+        type={type === "password" ? passwordVisible ? "text" : "password" : type}
         name={name}
         placeholder={placeholder}
         defaultValue={value}
         id={id}
         className='input-box'
-        />
-<i className="fi fi-user input-icon text-black"></i> 
-
-
+      />
+      {type === "password" && (
+        <div 
+          className='input-icon absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer'
+          onClick={() => setPasswordVisible(prev => !prev)}
+        >
+          {passwordVisible ? <FaEyeSlash className='text-xl text-gray-500 input-icon left-[auto] right-4' /> : <FaEye className='text-xl text-gray-500 input-icon left-[auto] right-4' />}
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,3 +1,4 @@
+import { FaUser, FaEnvelope, FaLock } from "react-icons/fa";
 import { InputBox } from "../components/input.component";
 
 const UserAuthForm = ({ type }) => {
@@ -5,36 +6,45 @@ const UserAuthForm = ({ type }) => {
         <section className="h-cover flex items-center justify-center">
             <form className="w-[80%] max-w-[400px]">
                 <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-                    {type == "sign-in" ? "Welcome back" : "Join us Today"}
+                    {type === "sign-in" ? "Welcome back" : "Join us Today"}
                 </h1>
 
-                {
-                    type != "sign-in" ?
-                        <InputBox
-                            name="fullname"
-                            type="text"
-                            id=""
-                            placeholder="Full Name"
-                        />
-                        : ""
-                }
+                {type !== "sign-in" && (
+                    <>
+                        <div className="relative">
+                            <InputBox
+                                name="fullname"
+                                type="text"
+                                id=""
+                                placeholder="Full Name"
+                            />
+                            <FaUser className="absolute left-4 top-1/2 transform -translate-y-1/2 text-xl text-gray-500" />
+                        </div>
+                    </>
+                )}
 
-                <InputBox
-                    name="email"
-                    type="email"
-                    id=""
-                    placeholder="Email "
-                />
-                <InputBox
-                    name="password"
-                    type="password"
-                    id=""
-                    placeholder="Password"
-                />
+                <div className="relative">
+                    <InputBox
+                        name="email"
+                        type="email"
+                        id=""
+                        placeholder="Email"
+                    />
+                    <FaEnvelope className="absolute left-4 top-1/2 transform -translate-y-1/2 text-xl text-gray-500" />
+                </div>
+
+                <div className="relative">
+                    <InputBox
+                        name="password"
+                        type="password"
+                        id=""
+                        placeholder="Password"
+                    />
+                    <FaLock className="absolute left-4 top-1/2 transform -translate-y-1/2 text-xl text-gray-500" />
+                </div>
             </form>
-
         </section>
-    )
+    );
 }
 
 export default UserAuthForm;
